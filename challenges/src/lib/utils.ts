e18642d97da259acab7d9e4166e1db521c544dac
Sougata <sougatadas9874@gmail.com>
2025-04-11 13:35:30 +0530

    add array utils test challenges

--- challenges/src/lib/utils.ts
+++ challenges/src/lib/utils.ts
@@ -35,3 +35,48 @@ export const isPalindrome = (str: string) => {
     reverseStr(str).toLowerCase()
   );
 };
+
+//array utils
+export const flattenArray = <T>(arr: (T | T[])[]): T[] => {
+  const newArr: T[] = []; //reinitialised on every recursive call.
+
+  arr.forEach((val) => {
+    if (Array.isArray(val)) {
+      //destructure sub array and then add to existing array
+      newArr.push(...flattenArray(val));
+    } else {
+      newArr.push(val);
+    }
+  });
+
+  return newArr;
+};
+
+//removes all duplicates but maintains structure
+export const removeDuplicates = <T>(arr: (T | T[])[]): (T | T[])[] => {
+  const newArr: (T | T[])[] = []; //reinitialised on every recursive call.
+
+  arr.forEach((val) => {
+    if (Array.isArray(val)) {
+      //add sub arrays as well
+      newArr.push(removeDuplicates(val) as T[]);
+    } else {
+      newArr.push(val);
+    }
+  });
+
+  //remove top level duplicates
+  return [...new Set(newArr)];
+};
+
+export const flattenAndRemoveDuplicates = <T>(arr: (T | T[])[]): T[] => {
+  const flattenedArray = flattenArray(arr);
+
+  return [...new Set(flattenedArray)];
+};
+
+export const intersectArray = (arr1: number[], arr2: number[]): number[] => {
+  return arr1.filter((item) => arr2.includes(item));
+};
+
+//concat flattens one level and then adds values, but push adds values as it is.
